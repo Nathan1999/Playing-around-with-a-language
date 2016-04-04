2697ee23650fe19526f6b65c221beafbaf382f5e
Nathan1999 <nathan.caplan234@gmail.com>
2016-04-04 21:14:57 +0100

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,19 @@
+from tkinter import * 
 
+
+class Application(frame):
+  def __init__(self, master):
+    Frame.__init__(self, master)
+    self.grid()
+    self.create_widgets()
+  
+  def create_widgets(self):
+    self.bollocks = Button(self, text = "bollocks")
+    self.bolocks.grid()
+    
+root = Tk()
+root.title = "buttons"
+root.geometry= ("200x500")
+
+app=Application(root)
+root.mainloop()
